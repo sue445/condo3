107821ec1fbca497ad140e940958ea2644c47ac1
sue445 <sue445@sue445.net>
2019-07-31 23:24:19 +0900

    Impl GetGroup

---
+++ model/doorkeeper/group.go
@@ -0,0 +1,52 @@
+package doorkeeper
+
+import (
+	"fmt"
+	"github.com/sue445/condo3/model"
+	"github.com/sue445/go-doorkeeper"
+	"os"
+	"time"
+)
+
+// GetGroup returns group detail
+func GetGroup(groupName string, currentTime time.Time) (*model.Group, error) {
+	client := doorkeeper.NewClient(os.Getenv("DOORKEEPER_ACCESS_TOKEN"))
+
+	doorkeeperGroup, _, err := client.GetGroup(groupName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	since := currentTime.AddDate(0, 0, -30*3)
+	until := currentTime.AddDate(0, 0, 30*3)
+	params := doorkeeper.GetEventsParams{
+		Sort:  doorkeeper.SortByPublishedAt,
+		Since: &since,
+		Until: &until,
+	}
+	doorkeeperEvents, _, err := client.GetGroupEvents(groupName, &params)
+
+	if err != nil {
+		return nil, err
+	}
+
+	group := model.Group{
+		Title:  fmt.Sprintf("%s | Doorkeeper", doorkeeperGroup.Name),
+		URL:    doorkeeperGroup.PublicURL,
+		Events: []model.Event{},
+	}
+
+	for _, ev := range doorkeeperEvents {
+		event := model.Event{
+			Title:     ev.Title,
+			URL:       ev.PublicURL,
+			Address:   ev.Address,
+			StartedAt: &ev.StartsAt,
+			EndedAt:   &ev.EndsAt,
+		}
+		group.Events = append(group.Events, event)
+	}
+
+	return &group, nil
+}
---
+++ model/doorkeeper/group_test.go
@@ -0,0 +1,86 @@
+package doorkeeper
+
+import (
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	"github.com/sue445/condo3/model"
+	"github.com/sue445/condo3/testutil"
+	"net/http"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func tp(t time.Time) *time.Time {
+	return &t
+}
+
+func TestGetGroup(t *testing.T) {
+	//ctx, done, err := aetest.NewContext()
+	//assert.NoError(t, err)
+	//defer done()
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", "https://api.doorkeeper.jp/groups/trbmeetup/events?&&&sort=published_at&since=2015-03-03&until=2015-08-30",
+		func(req *http.Request) (*http.Response, error) {
+			resp := httpmock.NewStringResponse(200, testutil.ReadTestData(filepath.Join("testdata", "events.json")))
+			resp.Header.Set("X-Ratelimit", `{"name":"authenticated API","period":300,"limit":300,"remaining":299,"until":"2019-07-29T15:15:00Z"}`)
+			return resp, nil
+		},
+	)
+	httpmock.RegisterResponder("GET", "https://api.doorkeeper.jp/groups/trbmeetup",
+		func(req *http.Request) (*http.Response, error) {
+			resp := httpmock.NewStringResponse(200, testutil.ReadTestData(filepath.Join("testdata", "group-ja.json")))
+			resp.Header.Set("X-Ratelimit", `{"name":"authenticated API","period":300,"limit":300,"remaining":299,"until":"2019-07-29T15:15:00Z"}`)
+			return resp, nil
+		},
+	)
+
+	type args struct {
+		groupName   string
+		currentTime time.Time
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantEventFirst model.Event
+		wantEventCount int
+		wantURL        string
+		wantTitle      string
+	}{
+		{
+			name: "successful",
+			args: args{
+				groupName:   "trbmeetup",
+				currentTime: time.Date(2015, 6, 1, 0, 0, 0, 0, time.UTC),
+			},
+			wantEventFirst: model.Event{
+				Title:     "900K records per second with Ruby, Java, and JRuby",
+				URL:       "https://trbmeetup.doorkeeper.jp/events/28319",
+				Address:   "東京都渋谷区神泉町8-16 渋谷ファーストプレイス8F",
+				StartedAt: tp(time.Date(2015, 8, 13, 10, 0, 0, 0, time.UTC)),
+				EndedAt:   tp(time.Date(2015, 8, 13, 13, 0, 0, 0, time.UTC)),
+			},
+			wantEventCount: 1,
+			wantURL:        "https://trbmeetup.doorkeeper.jp/",
+			wantTitle:      "Tokyo Rubyist Meetup | Doorkeeper",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetGroup(tt.args.groupName, tt.args.currentTime)
+
+			assert.NoError(t, err)
+			assert.NotNil(t, got)
+
+			if got != nil {
+				assert.Equal(t, tt.wantEventCount, len(got.Events))
+				assert.Equal(t, tt.wantEventFirst, got.Events[0])
+				assert.Equal(t, tt.wantURL, got.URL)
+				assert.Equal(t, tt.wantTitle, got.Title)
+			}
+		})
+	}
+}
