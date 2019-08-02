6b5e5ccf94e979627dcfd86dd9786cb518576dc5
sue445 <sue445@sue445.net>
2019-08-03 01:44:16 +0900

    http -> https

--- model/connpass/group_test.go
+++ model/connpass/group_test.go
@@ -27,7 +27,7 @@ func TestGetGroup(t *testing.T) {
 
 	httpmock.RegisterResponder("GET", "https://gocon.connpass.com/",
 		httpmock.NewStringResponder(200, testutil.ReadTestData("testdata/gocon.html")))
-	httpmock.RegisterResponder("GET", "http://connpass.com/api/v1/event/?count=100&order=3&series_id=312&ym=201902%2C201903%2C201904%2C201905%2C201906%2C201907%2C201908%2C201909%2C201910%2C201911%2C201912%2C202001%2C202002",
+	httpmock.RegisterResponder("GET", "https://connpass.com/api/v1/event/?count=100&order=3&series_id=312&ym=201902%2C201903%2C201904%2C201905%2C201906%2C201907%2C201908%2C201909%2C201910%2C201911%2C201912%2C202001%2C202002",
 		httpmock.NewStringResponder(200, testutil.ReadTestData("testdata/gocon.json")))
 
 	type args struct {
