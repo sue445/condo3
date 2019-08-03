7e8229b6aab7118d8eca5db4d0fec2df78cbceab
sue445 <sue445@sue445.net>
2019-08-04 02:02:00 +0900

    Use appengine.Main()
    ttps://github.com/gcpug/nouhau/issues/71

--- main.go
+++ main.go
@@ -15,13 +15,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/sue445/condo3/api"
+	"google.golang.org/appengine"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 )
 
@@ -45,14 +44,7 @@ func main() {
 	public := http.StripPrefix("/public", http.FileServer(http.Dir("public")))
 	http.Handle("/public/", public)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	appengine.Main()
 }
 
 // indexHandler uses a template to create an index.html.
