2566ba12f30ddcddf5c32220943d53ea21c6d006
sue445 <sue445@sue445.net>
2019-08-14 16:00:36 +0900

    Impl sandbox model

---
+++ model/sandbox/group.go
@@ -0,0 +1,130 @@
+package sandbox
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/sue445/condo3/model"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type group struct {
+	Title     string  `json:"title"`
+	URL       string  `json:"url"`
+	UpdatedAt string  `json:"updated_at"`
+	Events    []event `json:"events"`
+}
+
+type event struct {
+	Title       string `json:"title"`
+	URL         string `json:"url"`
+	Address     string `json:"address"`
+	UpdatedAt   string `json:"updated_at"`
+	PublishedAt string `json:"published_at"`
+	StartedAt   string `json:"started_at"`
+	EndedAt     string `json:"ended_at"`
+}
+
+// GetGroup returns group detail
+func GetGroup(groupName string) (*model.Group, error) {
+	url := fmt.Sprintf("https://sue445.github.io/condo3-sandbox/data/%s.json", groupName)
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
+		return nil, errors.New(resp.Status)
+	}
+
+	byteArray, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var res group
+	err = json.Unmarshal(byteArray, &res)
+
+	group := model.Group{
+		Title: res.Title,
+		URL:   res.URL,
+	}
+
+	if res.UpdatedAt != "" {
+		updatedAt, err := time.ParseInLocation(time.RFC3339, res.UpdatedAt, model.JST)
+
+		if err != nil {
+			return nil, err
+		}
+
+		group.UpdatedAt = &updatedAt
+	}
+
+	for _, e := range res.Events {
+		event, err := e.toEvent()
+
+		if err != nil {
+			return nil, err
+		}
+
+		group.Events = append(group.Events, *event)
+	}
+
+	return &group, nil
+}
+
+func (e *event) toEvent() (*model.Event, error) {
+	event := model.Event{
+		Title:   e.Title,
+		URL:     e.URL,
+		Address: e.Address,
+	}
+
+	if e.UpdatedAt != "" {
+		updatedAt, err := time.ParseInLocation(time.RFC3339, e.UpdatedAt, model.JST)
+
+		if err != nil {
+			return nil, err
+		}
+
+		event.UpdatedAt = &updatedAt
+	}
+
+	if e.PublishedAt != "" {
+		publishedAt, err := time.ParseInLocation(time.RFC3339, e.PublishedAt, model.JST)
+
+		if err != nil {
+			return nil, err
+		}
+
+		event.PublishedAt = &publishedAt
+	}
+
+	if e.StartedAt != "" {
+		startedAt, err := time.ParseInLocation(time.RFC3339, e.StartedAt, model.JST)
+
+		if err != nil {
+			return nil, err
+		}
+
+		event.StartedAt = &startedAt
+	}
+
+	if e.EndedAt != "" {
+		endedAt, err := time.ParseInLocation(time.RFC3339, e.EndedAt, model.JST)
+
+		if err != nil {
+			return nil, err
+		}
+
+		event.EndedAt = &endedAt
+	}
+
+	return &event, nil
+}
