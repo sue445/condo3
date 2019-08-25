37fce6af594250475ad35f81991f9677e66e0553
sue445 <sue445@sue445.net>
2019-08-25 09:34:36 +0900

    Impl logger.WithRequest

--- logger/logger.go
+++ logger/logger.go
@@ -1,9 +1,13 @@
 package logger
 
 import (
+	"contrib.go.opencensus.io/exporter/stackdriver/propagation"
+	"encoding/binary"
+	"encoding/hex"
 	"github.com/gelraen/appengine-formatter"
 	"github.com/sirupsen/logrus"
 	"io"
+	"net/http"
 	"os"
 	"runtime/debug"
 )
@@ -11,6 +15,12 @@ import (
 // NewLogger returns a new Logger instance
 func NewLogger() *logrus.Logger {
 	return newLogger(os.Stderr)
+}
+
+// NewRequestLogger returns a new Logger instance with http request
+func NewRequestLogger(r *http.Request) *logrus.Entry {
+	logger := newLogger(os.Stderr)
+	return WithRequest(logger, r)
 }
 
 func newLogger(out io.Writer) *logrus.Logger {
@@ -38,4 +48,21 @@ func newLogger(out io.Writer) *logrus.Logger {
 // SendError send error to Stackdriver Error Reporting
 func SendError(logger *logrus.Logger, err error) {
 	logger.Errorf("%v\n\n%s\n\n%+v", err, string(debug.Stack()), err)
+}
+
+// WithRequest add traceID and spanID to entry
+func WithRequest(logger *logrus.Logger, r *http.Request) *logrus.Entry {
+	var format = propagation.HTTPFormat{}
+
+	sc, ok := format.SpanContextFromRequest(r)
+
+	if !ok {
+		logger.Warn("FAILED: SpanContextFromRequest")
+		return logger.WithFields(logrus.Fields{})
+	}
+
+	return logger.WithFields(logrus.Fields{
+		"logging.googleapis.com/trace":  hex.EncodeToString(sc.TraceID[:]),
+		"logging.googleapis.com/spanId": binary.BigEndian.Uint64(sc.SpanID[:]),
+	})
 }
