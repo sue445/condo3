978f0e45f5770215197bf33337a06175c7fd553b
sue445 <sue445@sue445.net>
2021-04-16 00:02:16 +0900

    Tweak sentry span

--- api/connpass_handler.go
+++ api/connpass_handler.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"github.com/getsentry/sentry-go"
+	"github.com/gorilla/mux"
 	"github.com/sue445/condo3/model"
 	"github.com/sue445/condo3/model/connpass"
 	"net/http"
@@ -11,10 +12,11 @@ import (
 
 // ConnpassHandler returns handler of /api/conpass
 func (h *Handler) ConnpassHandler(w http.ResponseWriter, r *http.Request) {
-	h.performAPI(w, r, func(groupName string) (*model.Group, error) {
-		span := sentry.StartSpan(r.Context(), "/api/connpass/{group}", sentry.TransactionName(fmt.Sprintf("/api/connpass/%s", groupName)))
-		defer span.Finish()
+	vars := mux.Vars(r)
+	span := sentry.StartSpan(r.Context(), "/api/connpass/{group}.{format}", sentry.TransactionName(fmt.Sprintf("/api/connpass/%s.%s", vars["group"], vars["format"])))
+	defer span.Finish()
 
+	h.performAPI(w, r, func(groupName string) (*model.Group, error) {
 		sentry.ConfigureScope(func(scope *sentry.Scope) {
 			scope.SetTag("group", groupName)
 		})
